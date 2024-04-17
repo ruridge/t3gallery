238cdbdda5d54dd389c04e967b5a23e4ea6d663a
Robert Uridge <856071+ruridge@users.noreply.github.com>
2024-04-17 23:21:57 +0800

    Scaffold basic ui with mock data

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 ## Todo
 
-- [ ] Make it deploy (vercel)
-- [ ] Scaffold basic ui with mock data
+- [x] Make it deploy (vercel)
+- [x] Scaffold basic ui with mock data
 - [ ] Actually set up a database (vercel postgress)
 - [ ] Attach a database to UI
 - [ ] Add authentication (clerk)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/ced15ba2-db72-407e-b496-1f0f6281f71c-w5jsli.jpg",
+  "https://utfs.io/f/d5a40408-8bdf-492e-8d0c-5693ff0a3748-n4mtny.jpg",
+  "https://utfs.io/f/f6eeaf65-4163-4b7b-9592-145f114c29a6-30kk9c.jpg",
+  "https://utfs.io/f/2d2d8dee-e85f-49d7-bd1c-f6b02be5058b-p6zx8h.jpg",
+  "https://utfs.io/f/47f36605-0298-46f6-87f5-194e2f9e580f-36n3ff.jpg"
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url
+}))
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress)
+    <main>
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map(images => (
+          <div key={images.id} className="w-48">
+            <img src={images.url} alt="" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
