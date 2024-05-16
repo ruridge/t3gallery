b37916282bf6b483618880e6a8e03080ff0e3955
Robert Uridge <856071+ruridge@users.noreply.github.com>
2024-05-17 01:07:01 +0800

    update next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
-import { ClerkProvider } from "@clerk/nextjs";
 import "~/styles/globals.css";
+
+import { ClerkProvider } from "@clerk/nextjs";
 
 import { Inter } from "next/font/google";
 import TopNav from "./_components/topnav";
