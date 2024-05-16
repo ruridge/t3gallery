13e70dcb40d745090f3eba411873fa8da03b85c3
Robert Uridge <856071+ruridge@users.noreply.github.com>
2024-05-17 01:37:37 +0800

    add  publishableKey prop to ClerkProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
       <html lang="en">
         <body className={`font-sans ${inter.variable} flex flex-col gap-4`}>
           <TopNav />
